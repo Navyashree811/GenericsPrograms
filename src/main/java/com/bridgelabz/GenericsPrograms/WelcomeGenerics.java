9a5022cc022569ee544e24a81f6cc89bcc3aba60
hp-pc <navyashree811@gmail.com>
2022-08-18 16:26:47 +0530

    [NAVAYA]ADDED : Welcome to Generics Programs

---
+++ src/main/java/com/bridgelabz/GenericsPrograms/WelcomeGenerics.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.GenericsPrograms;
+
+public class WelcomeGenerics {  
+	public static void main(String[] args) {
+		System.out.println("Welcome to Generics Programs");
+	}
+
+}
