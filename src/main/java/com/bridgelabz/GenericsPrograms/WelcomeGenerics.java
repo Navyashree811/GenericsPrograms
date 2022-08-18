3ee471f50ac261e06674039baf9bd0a5810fe3ef
hp-pc <navyashree811@gmail.com>
2022-08-18 18:59:51 +0530

    [NAVAYA]ADDED : Given three Integer Find the Maximum

--- src/main/java/com/bridgelabz/GenericsPrograms/WelcomeGenerics.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.GenericsPrograms;
-
-public class WelcomeGenerics {  
-	public static void main(String[] args) {
-		System.out.println("Welcome to Generics Programs");
-	}
-
-}
